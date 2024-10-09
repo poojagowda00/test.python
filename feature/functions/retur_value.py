c3e5eff81aab18f79d57c566b8e38e5e67c69882
poojagowda00 <152789055+poojagowda00@users.noreply.github.com>
2024-10-09 13:20:32 +0000

    added new folder and created new files

---
+++ feature/functions/retur_value.py
@@ -0,0 +1,10 @@
+def addition (num1, num2):
+   add = num1 + num2
+   return add
+
+def subtraction (num1, num2):
+   sub = num1 - num2 
+   return sub
+
+print(addition(22 , 35))
+print(subtraction(50, 23)) 
